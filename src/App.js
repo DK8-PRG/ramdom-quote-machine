eb3b193b4a48c5d331696de05b5924544aaaa2c6
KIM <jakovinecko@gmail.com>
2019-06-25 13:57:07 +0200

    random quote machine
    fcc

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { random } from 'lodash';
+import 'typeface-roboto';
+import QuoteMachine from './components/QuoteMachine';
+import { Grid } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const styles = {
+  container: {
+    alignItems: 'center',
+    display: 'flex',
+    height: '100vh',
+  }
+};
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      quotes: [],
+      selectedQuoteIndex: null
+    }
+    this.assignNewQuoteIndex = this.assignNewQuoteIndex.bind(this);
+    this.selectedQuoteIndex = this.generateNewQuoteIndex.bind(this);
+  }
+
+  componentDidMount() {
+    fetch('https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json')
+      .then(data => data.json())
+      .then(quotes => this.setState({ quotes }, this.assignNewQuoteIndex));
+  }
+
+  get selectedQuote() {
+    if (!this.state.quotes.length || !Number.isInteger(this.state.selectedQuoteIndex)) {
+      return undefined; //03.4:43
+    }
+    return this.state.quotes[this.state.selectedQuoteIndex];
+  }
+  /**
+   * Returns an integer representing an index in state.quotes
+   * if state.quotes is empty, returns undefined
+   */
+  generateNewQuoteIndex() {//in terminal  npm install lodash, npm install @typescript-eslint/parser --save-dev
+    if (!this.state.quotes.length) {
+      return;
+    }
+    return random(0, this.state.quotes.length - 1);
+  }
+
+  assignNewQuoteIndex() {
+    this.setState({ selectedQuoteIndex: this.generateNewQuoteIndex() });
+  }
+
+
+  render() {
+    return (
+      <Grid className={this.props.classes.container} id="quote-box" justify="center" container >
+        <Grid xs={11} lg={8} item>
+          {
+            this.selectedQuote ? 
+            <QuoteMachine selectedQuote={this.selectedQuote} assignNewQuoteIndex={this.assignNewQuoteIndex} />: 
+            null}
+        </Grid>
+      </Grid>
+
+    );
+  }
 }
 
-export default App;
+export default withStyles(styles)(App);
+
+/**
+ * $ npm install @fortawesome/fontawesome-svg-core @fortawesome/free-brands-svg-icons @fortawesome/react-fontawesome
+ */
